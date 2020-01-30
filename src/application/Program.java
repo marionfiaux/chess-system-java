d69d31b802dd5c7d4ea098651cb03b081adcb593
Marion Fiaux <marion.fiaux@gmail.com>
2020-01-30 21:00:56 -0200

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+
+
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -21,7 +23,7 @@ public class Program {
 		while(true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -31,6 +33,10 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
